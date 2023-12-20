32cb0ca53326d4e756df6ac663cde74260ec1ab3
Saurabh Ranjan <inrsaurabh@gmail.com>
2023-12-20 21:49:17 +0530

    1. Folder structure created, will improve as required 2. parcel used

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import dotenv from "dotenv";
+
+import App from "./src/App";
+dotenv.config();
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                medium: "Open Sans Medium",
+                bold: "Open Sans Bold",
+                semibold: "Open Sans SemiBold",
+                light: "Open Sans Light",
+                regular: "Open Sans",
+            },
+        },
+    },
+    plugins: [],
+}
